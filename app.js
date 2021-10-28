9f5edf1c06ce98245c9a03f6a100924e8f36bcb9
Manel.Defnoune <36042435+manel009@users.noreply.github.com>
2021-10-28 14:57:07 +0200

    Modification des select en inputs

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
-function expandWidth(elementHtmlClique) {
-    elementHtmlClique.style.width = "500px";
+/* SELECT INGRDIENTS */
+function expandIngredients(idElementToExpand) {
+    let elementToExpand = document.getElementById(idElementToExpand);
+    elementToExpand.style.width = "350px";
+    elementToExpand.placeholder = "Recherche un ingérident";
+
+    let iconUp = document.getElementById('icon-ingredients-up');
+    iconUp.style.display = "block";
+
+    let iconDown = document.getElementById('icon-ingredients-down');
+    iconDown.style.display = "none";
+}
+
+function reduceIngredients(idElementToReduce) {
+    let elementToReduce = document.getElementById(idElementToReduce);
+    elementToReduce.style.width = "120px";
+    elementToReduce.placeholder = "";
+
+    let iconUp = document.getElementById('icon-ingredients-up');
+    iconUp.style.display = "none";
+
+    let iconDown = document.getElementById('icon-ingredients-down');
+    iconDown.style.display = "block";
+}
+
+/* SELECT APPAREILS */
+function expandAppareils(idElementToExpand) {
+    let elementToExpand = document.getElementById(idElementToExpand);
+    elementToExpand.style.width = "350px";
+    elementToExpand.placeholder = "Recherche un appareil";
+
+    let iconUp = document.getElementById('icon-appareils-up');
+    iconUp.style.display = "block";
+
+    let iconDown = document.getElementById('icon-appareils-down');
+    iconDown.style.display = "none";
+}
+
+function reduceAppareils(idElementToReduce) {
+    let elementToReduce = document.getElementById(idElementToReduce);
+    elementToReduce.style.width = "120px";
+    elementToReduce.placeholder = "";
+
+    let iconUp = document.getElementById('icon-appareils-up');
+    iconUp.style.display = "none";
+
+    let iconDown = document.getElementById('icon-appareils-down');
+    iconDown.style.display = "block";
+}
+
+
+/* SELECT USTENSILS */
+function expandUstensils(idElementToExpand) {
+    let elementToExpand = document.getElementById(idElementToExpand);
+    elementToExpand.style.width = "350px";
+    elementToExpand.placeholder = "Recherche un ustensil";
+
+    let iconUp = document.getElementById('icon-ustensils-up');
+    iconUp.style.display = "block";
+
+    let iconDown = document.getElementById('icon-ustensils-down');
+    iconDown.style.display = "none";
+}
+
+function reduceUstensils(idElementToReduce) {
+    let elementToReduce = document.getElementById(idElementToReduce);
+    elementToReduce.style.width = "120px";
+    elementToReduce.placeholder = "";
+
+    let iconUp = document.getElementById('icon-ustensils-up');
+    iconUp.style.display = "none";
+
+    let iconDown = document.getElementById('icon-ustensils-down');
+    iconDown.style.display = "block";
 }
