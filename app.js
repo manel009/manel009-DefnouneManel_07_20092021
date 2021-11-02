06f002120df940a4d57008b001696e59e1940f77
Manel.Defnoune <36042435+manel009@users.noreply.github.com>
2021-11-02 06:50:14 +0100

    Affiche select dynamique

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
 /* SELECT INGRDIENTS */
 function expandIngredients(idElementToExpand) {
+    // On change la taille de l'input
     let elementToExpand = document.getElementById(idElementToExpand);
     elementToExpand.style.width = "350px";
+    elementToExpand.style.borderBottomLeftRadius = 0;
     elementToExpand.placeholder = "Recherche un ingérident";
 
+    // On affiche la fleche vers le haut
     let iconUp = document.getElementById('icon-ingredients-up');
     iconUp.style.display = "block";
+    iconUp.style.borderBottomRightRadius = 0;
 
+    // On masque la fleche vers le bas
     let iconDown = document.getElementById('icon-ingredients-down');
     iconDown.style.display = "none";
+
+    // On affiche la zone avec tous les resultats 
+    let selectContent = document.getElementById('select-content-ingredients');
+    selectContent.style.display = "grid";
+    let selectContentWidth = elementToExpand.clientWidth + iconUp.clientWidth;
+    selectContent.style.width = selectContentWidth + "px";
+    selectContent.style.borderBottomRightRadius = "5px";
+    selectContent.style.borderBottomLeftRadius = "5px";
 }
 
 function reduceIngredients(idElementToReduce) {
+    // On reduit l'input
     let elementToReduce = document.getElementById(idElementToReduce);
     elementToReduce.style.width = "120px";
+    elementToReduce.style.borderBottomLeftRadius = "5px";
     elementToReduce.placeholder = "";
 
+    // On masque la fleche vers le haut
     let iconUp = document.getElementById('icon-ingredients-up');
     iconUp.style.display = "none";
 
+    // On affiche la fleche vers le bas
     let iconDown = document.getElementById('icon-ingredients-down');
     iconDown.style.display = "block";
+
+    // On masque le contenu du select
+    let selectContent = document.getElementById('select-content-ingredients');
+    selectContent.style.display = "none";
 }
 
 /* SELECT APPAREILS */
